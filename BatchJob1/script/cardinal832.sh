9a1ae4a5a6bc170e2cc7297f69488b98903bd556
vishal181718 <121867952+vishal181718@users.noreply.github.com>
2023-08-01 15:15:16 +0530

    Delete BatchJob1 directory
    deleted

--- BatchJob1/script/cardinal832.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source_file="/mnt/c/Users/VIMISE/OneDrive - Capgemini/Desktop/newworkspace/Job1/source/wmqfte/projects/CardinalHealth/data/fromcardinal/KROGERX12CAT.20230217.csv"
-destination_file="/mnt/c/Users/VIMISE/OneDrive - Capgemini/Desktop/newworkspace/Job1/desti/eprndata/cardinal/cost/"
-mv "$source_file" "$destination_file"
